0dd66146c9b9765be733be7de8e7a3d7ddbe6cc6
randyjhc <acad.randyjhc@gmail.com>
2025-05-05 20:05:42 -0700

    to test workflow

---
+++ docs/minify.js
@@ -0,0 +1,17 @@
+// minify.js
+const fs = require("fs");
+const { minify } = require("html-minifier-terser");
+
+(async () => {
+  const html = fs.readFileSync("index.html", "utf-8");
+
+  const minified = await minify(html, {
+    collapseWhitespace: true,
+    removeComments: true,
+    minifyCSS: true,
+    minifyJS: true,
+  });
+
+  fs.writeFileSync("dist/index.html", minified, "utf-8");
+  console.log("✅ Minified HTML");
+})();
